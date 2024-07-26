4bc8f0aa4160f332eaa7d91520e9ff7932d8f9c2
i <2607587+isbit@users.noreply.github.com>
2024-07-26 13:08:25 +0200

    Zoom granularity, speed and scroll direction

--- content_script.js
+++ content_script.js
@@ -1,6 +1,6 @@
 (function () {
     document.addEventListener('wheel', onWheel, { passive: false })
-
+    var slowdown = 1
     function onWheel(event) {
         // Check that cmd key is held
         if (event.metaKey) {
@@ -9,10 +9,15 @@
 
             // mouse scroll forward (towards front of the mouse) is negative,
             // backwards (towards rear of mouse) is positive
-            const zoomIn = event.deltaY <= 0
+            const zoomIn = event.deltaY >= 0
 
             // send message to background script
-            chrome.runtime.sendMessage({zoomIn})
+            if (slowdown) {
+                chrome.runtime.sendMessage({zoomIn})
+                slowdown = 0
+            } else {
+                slowdown = 1
+            }
         }
     }
 })()
